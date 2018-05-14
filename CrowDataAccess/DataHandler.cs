32dcb739e092e4eb31963c54773bda8e1e40151c
CyberCyclistMSN <CyberCyclist@msn.com>
2018-05-14 17:03:03 -0500

    Add files via upload

---
+++ CrowDataAccess/DataHandler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CroweDataAccess
+{
+    public class DataHandler
+    {
+		public string RetrieveData()
+		{
+			return "Hello World!";
+		}
+		public bool WriteData(IDataWriter writer)
+		{
+			return writer.Write(RetrieveData());
+		}
+    }
+}
---
+++ CrowDataAccess/IDataWriter.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CroweDataAccess
+{
+    public interface IDataWriter
+    {
+		bool Write(string data);
+    }
+}
